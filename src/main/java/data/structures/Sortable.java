e80d1c0b20121e15eced5580c63bcc8785eb3797
Davi Carrer <vittidavii@gmail.com>
2022-10-19 23:33:08 -0300

    Sortable com métodos de ordenação por Selection e Bubble

--- src/main/java/data/structures/Sortable.java
+++ src/main/java/data/structures/Sortable.java
@@ -2,5 +2,9 @@ package data.structures;
 
 public interface Sortable<T extends Comparable<T>> {
 
-    T[] sort(T[] elements);
+    int[] selectionSort(int[] elements);
+    int[] bubbleSort(int[] elements);
+    int[] mergeSort(int[] elements);
+    int[] radixSort(int[] elements);
+    int[] insertionSort(int[] elements);
 }
